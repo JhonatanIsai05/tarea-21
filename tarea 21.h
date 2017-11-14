99c991184fd2ebdffc4f49d9a39a50015e67fb8f
Jhonatan Isaí Ramírez Álvarez <31527722+JhonatanIsai05@users.noreply.github.com>
2017-11-14 01:03:44 -0600

    Create tarea 21.h

---
+++ tarea 21.h
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+void fnj(){
+	printf("JJJJ\n");
+	printf("  J \n");
+	printf("  J \n");
+	printf("  J \n");
+	printf("JJJ \n");
+	}	
+void fnh(){
+	printf("H  H\n");
+	printf("H  H\n");
+	printf("HHHH\n");
+	printf("H  H\n");
+	printf("H  H\n");
+}
+void fno(){
+	printf("OOOO\n");
+	printf("O  O\n");
+	printf("O  O\n");
+	printf("O  O\n");
+	printf("OOOO\n");
+}
+void fnn(){
+	printf("N  N\n");
+	printf("NN N\n");
+	printf("N NN\n");
+	printf("N  N\n");
+	printf("N  N\n");
+}
+void fna(){
+	printf("  A \n");
+	printf(" A A\n");
+	printf(" AAA \n");
+	printf("A   A\n");
+	printf("A   A\n");
+}
+void fnt(){
+	printf("TTTT\n");
+	printf("  T \n");
+	printf("  T \n");
+	printf("  T \n");
+	printf("  T \n");
+}
